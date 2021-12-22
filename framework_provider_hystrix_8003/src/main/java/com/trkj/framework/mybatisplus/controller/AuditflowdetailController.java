0844827b24efd1bd93ef76a97308a657c244311b
liyu <dx112729@163.com>
2021-12-22 14:44:35 +0800

    完善

--- framework_provider_hystrix_8003/src/main/java/com/trkj/framework/mybatisplus/controller/AuditflowdetailController.java
+++ framework_provider_hystrix_8003/src/main/java/com/trkj/framework/mybatisplus/controller/AuditflowdetailController.java
@@ -2,19 +2,20 @@ package com.trkj.framework.mybatisplus.controller;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
- * 员工表 前端控制器
+ * 审批流明细表 前端控制器
  * </p>
  *
- * @author 劉祁
+ * @author 里予
  * @since 2021-12-22
  */
 @RestController
-@RequestMapping("/mybatisplus/staff")
-public class StaffController {
+@RequestMapping("/mybatisplus/auditflowdetail")
+public class AuditflowdetailController {
 
 }
 
