d52784266641c03715ef67633c18a32ad1b0f39b
chendou <1427760246@qq.com>
2021-12-22 16:27:24 +0800

    完善

--- framework_provider_hystrix_8008/src/test/java/MybatisPlusCode.java
+++ framework_provider_hystrix_8008/src/test/java/MybatisPlusCode.java
@@ -74,7 +74,7 @@ public class MybatisPlusCode {
         //4、策略配置
         StrategyConfig strategyConfig = new StrategyConfig();
         //数据库表名
-        strategyConfig.setInclude("PUNISH");
+        strategyConfig.setInclude("WORKER");
         //包的命名规则
         strategyConfig.setNaming(NamingStrategy.underline_to_camel);
         //列的名字
