ac9fc461558e85482da194a1c55a6cd70c14f126
liyu <dx112729@163.com>
2021-12-29 16:48:15 +0800

    完善

--- framework_provider_hystrix_8003/src/main/java/com/trkj/framework/mybatisplus/mapper/AuditflowMapper.java
+++ framework_provider_hystrix_8003/src/main/java/com/trkj/framework/mybatisplus/mapper/AuditflowMapper.java
@@ -1,11 +1,15 @@
 package com.trkj.framework.mybatisplus.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.trkj.framework.entity.mybatisplus.Auditflow;
+import com.trkj.framework.entity.mybatisplus.RegisterLog;
 import com.trkj.framework.vo.Auditflowone;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.HashMap;
@@ -20,25 +24,19 @@ import java.util.List;
  * @since 2021-12-27
  */
 @Mapper
-public interface AuditflowMapper extends BaseMapper<Auditflow> {
+public interface AuditflowMapper extends BaseMapper<Auditflowone> {
 
-    @Select("select a.CREATED_TIME,a.AUDITFLOW_ID,a.AUDITFLOW_TYPE,a.STAFF_NAME as STAFF_NAME1,a.AUDITFLOW_STATE,b.STAFF_NAME as STAFF_NAME2,b.UPDATED_TIME " +
-            "from AUDITFLOW a LEFT OUTER join AUDITFLOWDETAIL b " +
-            "on a.AUDITFLOW_ID=b.AUDITFLOW_ID " +
-            "where b.STAFF_NAME='部门经理' and b.AUDITFLOWDETAI_STATE=1  and a.AUDITFLOW_TYPE = '加班' ")
-    IPage<Auditflowone> one(Page<Auditflowone> page);
+    @Select("select a.CREATED_TIME,a.AUDITFLOW_ID,a.AUDITFLOW_TYPE,a.STAFF_NAME as STAFF_NAME1,b.AUDITFLOWDETAI_STATE,b.STAFF_NAME as STAFF_NAME2,b.UPDATED_TIME from AUDITFLOW a LEFT OUTER join AUDITFLOWDETAIL b on a.AUDITFLOW_ID=b.AUDITFLOW_ID ${ew.customSqlSegment}")
+    IPage<Auditflowone> selectAuditflowoneAll(Page<Auditflowone> page,@Param(Constants.WRAPPER) QueryWrapper<Auditflowone> queryWrapper);
 
-    @Select("select a.CREATED_TIME,a.AUDITFLOW_ID,a.AUDITFLOW_TYPE,a.STAFF_NAME as STAFF_NAME1,a.AUDITFLOW_STATE,b.STAFF_NAME as STAFF_NAME2,b.UPDATED_TIME " +
-            "from AUDITFLOW a LEFT OUTER join AUDITFLOWDETAIL b " +
-            "on a.AUDITFLOW_ID=b.AUDITFLOW_ID " +
-            "where b.STAFF_NAME='部门经理' and b.AUDITFLOWDETAI_STATE<>1 and b.AUDITFLOWDETAI_STATE<>0 and a.AUDITFLOW_TYPE='加班' ")
-    IPage<Auditflowone> two(Page<Auditflowone> page);
+    @Select("select a.CREATED_TIME,a.AUDITFLOW_ID,a.AUDITFLOW_TYPE,a.STAFF_NAME as STAFF_NAME1,a.AUDITFLOW_STATE,b.STAFF_NAME as STAFF_NAME2,b.UPDATED_TIME from AUDITFLOW a LEFT OUTER join AUDITFLOWDETAIL b on a.AUDITFLOW_ID=b.AUDITFLOW_ID ${ew.customSqlSegment}")
+    IPage<Auditflowone> selectEnddAuditflow(Page<Auditflowone> page,@Param(Constants.WRAPPER) QueryWrapper<Auditflowone> queryWrapper);
 
-    @Select("select a.AUDITFLOW_TITLE,o.STAFF_NAME as STAFF_NAME1,b.AUDITFLOWDETAI_STATE,b.STAFF_NAME as STAFF_NAME2,b.AUDITFLOWDETAI_REMARKS,b.AUDITFLOWDETAI_DATE,o.OVERTIMEASK_TYPE,o.OVERTIMEASK_MATTER,o.OVERTIMEASK_REMARKS,o.OVERTIMEASK_S_DATE,o.OVERTIMEASK_E_DATE,o.OVERTIMEASK_TOTAL_DATE " +
-            "from AUDITFLOW a LEFT JOIN AUDITFLOWDETAIL b on a.AUDITFLOW_ID=b.AUDITFLOW_ID\n" +
-            "LEFT JOIN OVERTIMEASK o on a.AUDITFLOW_ID=b.AUDITFLOW_ID " +
-            "where a.AUDITFLOW_ID=? " +
-            "and o.STAFF_NAME=? " +
-            "and b.STAFF_NAME=?")
-    List<Auditflowone> selectDetailsAuditflow(HashMap<String, Object> map);
+    // @Select("select a.AUDITFLOW_TITLE,o.STAFF_NAME as STAFF_NAME1,b.AUDITFLOWDETAI_STATE,b.STAFF_NAME as STAFF_NAME2,b.AUDITFLOWDETAI_REMARKS,b.AUDITFLOWDETAI_DATE,o.OVERTIMEASK_TYPE,o.OVERTIMEASK_MATTER,o.OVERTIMEASK_REMARKS,o.OVERTIMEASK_S_DATE,o.OVERTIMEASK_E_DATE,o.OVERTIMEASK_TOTAL_DATE " +
+    //         "from AUDITFLOW a LEFT JOIN AUDITFLOWDETAIL b on a.AUDITFLOW_ID=b.AUDITFLOW_ID\n" +
+    //         "LEFT JOIN OVERTIMEASK o on a.AUDITFLOW_ID=b.AUDITFLOW_ID " +
+    //         "where a.AUDITFLOW_ID=? " +
+    //         "and o.STAFF_NAME=? " +
+    //         "and b.STAFF_NAME=?")
+    // List<Auditflowone> selectDetailsAuditflow(HashMap<String, Object> map);
 }
