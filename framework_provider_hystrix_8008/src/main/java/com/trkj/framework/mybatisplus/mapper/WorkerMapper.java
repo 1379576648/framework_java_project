780b0e3fe8326b8c1949eee83ea7a0fb8b0ba212
袁可颖 <3043949730@qq.com>
2021-12-29 21:30:21 +0800

    完善

---
+++ framework_provider_hystrix_8008/src/main/java/com/trkj/framework/mybatisplus/mapper/WorkerMapper.java
@@ -0,0 +1,18 @@
+package com.trkj.framework.mybatisplus.mapper;
+
+import com.trkj.framework.entity.mybatisplus.Worker;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ * 转正 Mapper 接口
+ * </p>
+ *
+ * @author suki
+ * @since 2021-12-29
+ */
+@Mapper
+public interface WorkerMapper extends BaseMapper<Worker> {
+
+}
---
+++ framework_provider_hystrix_8008/src/main/java/com/trkj/framework/mybatisplus/service/impl/WorkerServiceImpl.java
@@ -0,0 +1,20 @@
+package com.trkj.framework.mybatisplus.service.impl;
+
+import com.trkj.framework.entity.mybatisplus.Worker;
+import com.trkj.framework.mybatisplus.mapper.WorkerMapper;
+import com.trkj.framework.mybatisplus.service.WorkerService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 转正 服务实现类
+ * </p>
+ *
+ * @author suki
+ * @since 2021-12-29
+ */
+@Service
+public class WorkerServiceImpl extends ServiceImpl<WorkerMapper, Worker> implements WorkerService {
+
+}
