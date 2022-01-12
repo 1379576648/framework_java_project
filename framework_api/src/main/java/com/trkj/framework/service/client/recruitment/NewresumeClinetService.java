edbcd784604d29db846e07f4bb2da02d16ef934e
tanwei <1713984131@qq.com>
2022-01-12 16:57:37 +0800

    招聘完善

--- framework_api/src/main/java/com/trkj/framework/service/client/recruitment/NewresumeClinetService.java
+++ framework_api/src/main/java/com/trkj/framework/service/client/recruitment/NewresumeClinetService.java
@@ -2,6 +2,7 @@ package com.trkj.framework.service.client.recruitment;
 
 import com.trkj.framework.service.client.fallbackfactory.NewresumeClinetServiceFallbackfactory;
 import com.trkj.framework.service.client.fallbackfactory.RegisterClinetServiceFallbackfactory;
+import com.trkj.framework.vo.InterviewVo;
 import com.trkj.framework.vo.RecruitmentVo;
 import com.trkj.framework.vo.ResumeVo;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -12,18 +13,27 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(value = "REGISTER-8010/provider", fallbackFactory = NewresumeClinetServiceFallbackfactory.class)
 public interface NewresumeClinetService {
+    // 新简历
     @PostMapping("/selectResume")
     Object queryResume(@RequestBody ResumeVo resumeVo);
 
+    // 全部简历
     @PostMapping("/selectAllresume")
     Object queryAll(@RequestBody ResumeVo resumeVo);
 
+    // 候选人
     @PostMapping("/selectCandidate")
     Object queryCandidate(@RequestBody ResumeVo resumeVo);
 
+    // 淘汰库
     @PostMapping("/selectEliminate")
     Object queryEliminate(@RequestBody ResumeVo resumeVo);
 
+    // 招聘计划查询
     @PostMapping("/selectRecruitment")
     Object queryRecruitment(@RequestBody RecruitmentVo recruitmentVo);
+
+    // 面试通过查询
+    @PostMapping("/selectInterviewPass")
+    Object queryInterviewPass(@RequestBody InterviewVo interviewVo);
 }
--- framework_consumer_feign_80/src/main/java/com/trkj/framework/controller/InterviewController.java
+++ framework_consumer_feign_80/src/main/java/com/trkj/framework/controller/InterviewController.java
@@ -2,20 +2,20 @@ package com.trkj.framework.controller;
 
 import com.trkj.framework.service.client.recruitment.NewresumeClinetService;
 import com.trkj.framework.vo.AjaxResponse;
-import com.trkj.framework.vo.RecruitmentVo;
+import com.trkj.framework.vo.InterviewVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class RecruitmentController {
+public class InterviewController {
     @Autowired
     private NewresumeClinetService newresumeClinetService=null;
 
-    // 招聘计划查询
-    @PostMapping("/selectRecruitment")
-    private Object queryRecruitment(@RequestBody RecruitmentVo recruitmentVo){
-        return AjaxResponse.success(newresumeClinetService.queryRecruitment(recruitmentVo));
+    //面试通过查询
+    @PostMapping("/selectInterviewPass")
+    public Object queryInterviewPass(@RequestBody InterviewVo interviewVo){
+        return AjaxResponse.success(newresumeClinetService.queryInterviewPass(interviewVo));
     }
 }
