78bf6f96acdbb0492c653ffd80741655e54a8856
liyu <dx112729@163.com>
2022-02-26 15:24:14 +0800

    完善

--- framework_consumer_feign_80/src/main/java/com/trkj/framework/controller/CardController.java
+++ framework_consumer_feign_80/src/main/java/com/trkj/framework/controller/CardController.java
@@ -4,6 +4,7 @@ import com.trkj.framework.entity.mybatisplus.Card;
 import com.trkj.framework.service.client.examine.AuditflowService;
 import com.trkj.framework.util.CarryTokenUtil;
 import com.trkj.framework.vo.*;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +21,7 @@ public class CardController {
     private AuditflowService auditflowService;
     @Autowired
     private CarryTokenUtil carryTokenUtil;
+
     /**
      * 根据审批类型的补打卡/审批人查询待处理的审批
      *
@@ -27,8 +29,9 @@ public class CardController {
      * @return
      */
     @PostMapping("/selectCardAll")
+    @ApiOperation(value = "查询待处理的补打卡审批", notes = "审批模块", httpMethod = "POST", nickname = "查询", produces = "/selectCardAll")
     public AjaxResponse selectCardAll(@RequestBody Auditflowone auditflowone) {
-        Map<String,Object> map = (Map<String, Object>)auditflowService.selectCardAll(auditflowone);
+        Map<String, Object> map = (Map<String, Object>) auditflowService.selectCardAll(auditflowone);
         return AjaxResponse.success(carryTokenUtil.main(map));
     }
 
@@ -39,8 +42,9 @@ public class CardController {
      * @return
      */
     @PostMapping("/selectEndCardAll")
+    @ApiOperation(value = "查询已处理的补打卡审批", notes = "审批模块", httpMethod = "POST", nickname = "查询", produces = "/selectEndCardAll")
     public AjaxResponse selectEndCardAll(@RequestBody Auditflowone auditflowone) {
-        Map<String,Object> map = (Map<String, Object>)auditflowService.selectEndCardAll(auditflowone);
+        Map<String, Object> map = (Map<String, Object>) auditflowService.selectEndCardAll(auditflowone);
         return AjaxResponse.success(carryTokenUtil.main(map));
     }
 
@@ -51,52 +55,61 @@ public class CardController {
      * @return
      */
     @PostMapping("/selectDetailsCards")
+    @ApiOperation(value = "查询已处理的补打卡审批详情信息", notes = "审批模块", httpMethod = "POST", nickname = "查询", produces = "/selectDetailsCards")
     public AjaxResponse selectDetailsCards(@RequestBody CardDetailsVo cardDetailsVo) {
-        Map<String,Object> map = (Map<String, Object>)auditflowService.selectDetailsCards(cardDetailsVo);
+        Map<String, Object> map = (Map<String, Object>) auditflowService.selectDetailsCards(cardDetailsVo);
         return AjaxResponse.success(carryTokenUtil.main(map));
     }
 
     /**
      * 根据员工名称是否有补打卡记录
+     *
      * @param cardDetailsVo
      * @return
      */
     @PostMapping("/selectCardExamine")
-    public AjaxResponse selectCardExamine(@RequestBody CardDetailsVo cardDetailsVo){
-        Map<String,Object> map = (Map<String, Object>)auditflowService.selectCardExamine(cardDetailsVo);
+    @ApiOperation(value = "根据员工名称是否有补打卡记录", notes = "审批模块", httpMethod = "POST", nickname = "查询", produces = "/selectCardExamine")
+    public AjaxResponse selectCardExamine(@RequestBody CardDetailsVo cardDetailsVo) {
+        Map<String, Object> map = (Map<String, Object>) auditflowService.selectCardExamine(cardDetailsVo);
         return AjaxResponse.success(carryTokenUtil.main(map));
     }
 
     /**
      * 添加补打卡 三个审批人
+     *
      * @param cardVo
      * @return
      */
     @PostMapping("/submitToCard3")
-    public AjaxResponse submitToCard3(@RequestBody CardVo cardVo){
-        Map<String,Object> map = (Map<String, Object>)auditflowService.submitToCard3(cardVo);
+    @ApiOperation(value = "添加补打卡 三个审批人", notes = "审批模块", httpMethod = "POST", nickname = "添加", produces = "/submitToCard3")
+    public AjaxResponse submitToCard3(@RequestBody CardVo cardVo) {
+        Map<String, Object> map = (Map<String, Object>) auditflowService.submitToCard3(cardVo);
         return AjaxResponse.success(carryTokenUtil.main(map));
     }
 
     /**
      * 添加补打卡 两个审批人
+     *
      * @param cardVo
      * @return
      */
     @PostMapping("/submitToCard2")
-    public AjaxResponse submitToCard2(@RequestBody CardVo cardVo){
-        Map<String,Object> map = (Map<String, Object>)auditflowService.submitToCard2(cardVo);
+    @ApiOperation(value = "添加补打卡 两个审批人", notes = "审批模块", httpMethod = "POST", nickname = "添加", produces = "/submitToCard2")
+    public AjaxResponse submitToCard2(@RequestBody CardVo cardVo) {
+        Map<String, Object> map = (Map<String, Object>) auditflowService.submitToCard2(cardVo);
         return AjaxResponse.success(carryTokenUtil.main(map));
     }
 
     /**
      * 添加补打卡 一个审批人
+     *
      * @param cardVo
      * @return
      */
     @PostMapping("/submitToCard1")
-    public AjaxResponse submitToCard1(@RequestBody CardVo cardVo){
-        Map<String,Object> map = (Map<String, Object>)auditflowService.submitToCard1(cardVo);
+    @ApiOperation(value = "添加补打卡 一个审批人", notes = "审批模块", httpMethod = "POST", nickname = "添加", produces = "/submitToCard1")
+    public AjaxResponse submitToCard1(@RequestBody CardVo cardVo) {
+        Map<String, Object> map = (Map<String, Object>) auditflowService.submitToCard1(cardVo);
         return AjaxResponse.success(carryTokenUtil.main(map));
     }
 }
--- framework_consumer_feign_80/src/main/java/com/trkj/framework/controller/TraveController.java
+++ framework_consumer_feign_80/src/main/java/com/trkj/framework/controller/TraveController.java
@@ -4,6 +4,7 @@ import com.trkj.framework.entity.mybatisplus.Travel;
 import com.trkj.framework.service.client.examine.AuditflowService;
 import com.trkj.framework.util.CarryTokenUtil;
 import com.trkj.framework.vo.*;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,6 +26,7 @@ public class TraveController {
      * @return
      */
     @PostMapping("/selectTravelAll")
+    @ApiOperation(value = "根据审批类型的出差/审批人查询待处理的审批", notes = "审批模块", httpMethod = "POST", nickname = "查询", produces = "/selectTravelAll")
     public AjaxResponse selectTravelAll(@RequestBody Auditflowone auditflowone) {
         Map<String, Object> map = (Map<String, Object>) auditflowService.selectTravelAll(auditflowone);
         return AjaxResponse.success(carryTokenUtil.main(map));
@@ -37,6 +39,7 @@ public class TraveController {
      * @return
      */
     @PostMapping("/selectEndTravelAll")
+    @ApiOperation(value = "根据审批类型的出差/审批人查询已处理的审批", notes = "审批模块", httpMethod = "POST", nickname = "查询", produces = "/selectEndTravelAll")
     public AjaxResponse selectEndTravelAll(@RequestBody Auditflowone auditflowone) {
         Map<String, Object> map = (Map<String, Object>) auditflowService.selectEndTravelAll(auditflowone);
         return AjaxResponse.success(carryTokenUtil.main(map));
@@ -49,6 +52,7 @@ public class TraveController {
      * @return
      */
     @PostMapping("/selectDetailsTrave")
+    @ApiOperation(value = "审批人查询已处理的详情信息", notes = "审批模块", httpMethod = "POST", nickname = "查询", produces = "/selectDetailsTrave")
     public AjaxResponse selectDetailsTrave(@RequestBody TravelDetailsVo travelDetailsVo) {
         Map<String, Object> map = (Map<String, Object>) auditflowService.selectDetailsTrave(travelDetailsVo);
         return AjaxResponse.success(carryTokenUtil.main(map));
@@ -61,6 +65,7 @@ public class TraveController {
      * @return
      */
     @PostMapping("/selectEvectionExamine")
+    @ApiOperation(value = "根据员工名称是否有出差记录", notes = "审批模块", httpMethod = "POST", nickname = "查询", produces = "/selectEvectionExamine")
     public AjaxResponse selectEvectionExamine(@RequestBody TravelDetailsVo travelDetailsVo) {
         Map<String, Object> map = (Map<String, Object>) auditflowService.selectEvectionExamine(travelDetailsVo);
         return AjaxResponse.success(carryTokenUtil.main(map));
@@ -73,6 +78,7 @@ public class TraveController {
      * @return
      */
     @PostMapping("/submitToTravel3")
+    @ApiOperation(value = "添加出差 三个审批人", notes = "审批模块", httpMethod = "POST", nickname = "添加", produces = "/submitToTravel3")
     public AjaxResponse submitToTravel3(@RequestBody TravelVo travelVo) {
         Map<String, Object> map = (Map<String, Object>) auditflowService.submitToTravel3(travelVo);
         return AjaxResponse.success(carryTokenUtil.main(map));
@@ -85,6 +91,7 @@ public class TraveController {
      * @return
      */
     @PostMapping("/submitToTravel2")
+    @ApiOperation(value = "添加出差 两个审批人", notes = "审批模块", httpMethod = "POST", nickname = "添加", produces = "/submitToTravel2")
     public AjaxResponse submitToTravel2(@RequestBody TravelVo travelVo) {
         Map<String, Object> map = (Map<String, Object>) auditflowService.submitToTravel2(travelVo);
         return AjaxResponse.success(carryTokenUtil.main(map));
@@ -97,6 +104,7 @@ public class TraveController {
      * @return
      */
     @PostMapping("/submitToTravel1")
+    @ApiOperation(value = "添加出差 添加一个审批人", notes = "审批模块", httpMethod = "POST", nickname = "添加", produces = "/submitToTravel1")
     public AjaxResponse submitToTravel1(@RequestBody TravelVo travelVo) {
         Map<String, Object> map = (Map<String, Object>) auditflowService.submitToTravel1(travelVo);
         return AjaxResponse.success(carryTokenUtil.main(map));
